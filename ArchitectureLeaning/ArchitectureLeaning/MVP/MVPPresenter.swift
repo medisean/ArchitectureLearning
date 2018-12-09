3f4d7edb003185b6ebfb3ad25e5b2c0c133e8144
zouliangming <liangmingzou@163.com>
2018-12-10 00:14:55 +0800

    update mvp mode

---
+++ ArchitectureLeaning/ArchitectureLeaning/MVP/MVPPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  MVPPresenter.swift
+//  ArchitectureLeaning
+//
+//  Created by 邹亮明 on 2018/12/8.
+//  Copyright © 2018 zlm. All rights reserved.
+//
+
+import Foundation
+
+protocol GetDataSuccess {
+    func onGetDataSuccess()
+}
+
+class MVPPresenter {
+    var view: UIView?
+    init(view: UIView) {
+        
+    }
+}
